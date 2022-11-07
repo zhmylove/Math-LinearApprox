888d73bcad345185240e120ba97d4a9f1d5adf08
zhmylove <zhmylove@narod.ru>
2022-11-08 01:01:21 +0300

    Implement deltas calculation

--- generate_line.pl
+++ generate_line.pl
@@ -1,14 +1,12 @@
 #!/usr/bin/perl
 # made by: KorG
 # Example usage:
-# ./generate_line.pl --A=8 --B=7 --step=0.3 --count=1000000 | ./LinearApprox
+# ./generate_line.pl --A=8 --B=7 --step=0.3 --x0=-5.5 --count=1000000 > file
+# ./LinearApprox < file
 
 use strict;
-use v5.18;
 use warnings;
-no warnings 'experimental';
-use utf8;
-binmode STDOUT, ':utf8';
+use Getopt::Long;
 
 sub usage {
    warn "Usage: $0 <--A=NUM --B=NUM> [OPTIONS]
@@ -18,36 +16,65 @@ sub usage {
    --seed      Random seed
    --spread    Height of random spread (y = +- spread / 2)
    --step      Average step between points
-   --x0        Starting point on X axis"
-   . "\n";
+   --x0        Starting point on X axis
+   \n";
    q();
 }
 
-use Getopt::Long;
-my ($A, $B);
-my $count = 10;
-my $seed;
-my $spread = 10;
-my $step = 1;
-my $x = 0;
-GetOptions (
-   "A=i" => \$A,
-   "B=i" => \$B,
-   "count=i" => \$count,
-   "seed=i" => \$seed,
-   "spread=s" => \$spread,
-   "step=s" => \$step,
-   "x0=s" => \$x,
-) or die usage;
+my %opts;
+GetOptions(
+   \%opts, qw( A=s B=s count=i seed=s spread=s step=s x0=s )
+);
 
+# Prepare frequently used variables
+my ($spread, $step) = (0, 1);
+
+# Assign mandatory parameters
+my ($A, $B) = @opts{qw( A B )};
+
+# Assign optional parameters
+$spread = $opts{spread} if defined $opts{spread};
+$step = $opts{step} if defined $opts{step};
+
+# Append default values
+%opts = (
+   count => 10,
+   x0 => 0,
+   %opts
+);
+
+# Check if the user specified A and B
 die usage unless defined $A and defined $B;
 
-my $y;
-srand($seed) if defined $seed;
+# Prepare math engine
+my ($x, $y) = $opts{x0};
+my $count = $opts{count};
+srand($opts{seed}) if defined $opts{seed};
 
-while ($count-- >= 0) {
+# Stuff for deltas -- to estimate A deviation due to random spread
+my $real_delta_size = 10;
+my $real_delta_divider = int($count / $real_delta_size);
+my (@real_deltas);
+
+while ($count > 0) {
    $y = $A * $x + $B;
-   $y += rand($spread) - $spread / 2 if $spread > 0;
+
+   # If spread is required, add it to Y and save into real_deltas by index
+   if ($spread > 0) {
+      my $delta = rand($spread) - $spread / 2;
+      $real_deltas[int($count / $real_delta_divider)] += $delta;
+      $y += $delta;
+   }
+
+   # Print to stdout
    print "$x $y\n";
+
+   # Prepare for the next step
    $x += $step;
+   $count--;
+}
+
+# Print delta statistics if there was some random in data
+if ($spread > 0) {
+   warn join("\n", "Real deltas: ", reverse(@real_deltas)) . "\n";
 }
